985e9732b5c009a0f8ad746353687ec0ed1587cd
Animesh Kumar Singh <animesh.workplace@gmail.com>
2025-01-28 18:45:09 +0530

    updated file - backend/query_engine/add_Script.py

--- backend/query_engine/add_Script.py
+++ backend/query_engine/add_Script.py
@@ -17,7 +17,6 @@ def bulk_insert(chunk):
             # Create ChromosomeRegion objects
             chromosome_regions = []
             tf_chipseq_objects = []
-
             for _, row in chunked_df.iterrows():
                 chr_region = ChromosomeRegion(
                     chr=row["Chr"],
@@ -26,12 +25,10 @@ def bulk_insert(chunk):
                     type="VaraDB_TFChipSEQ",
                 )
                 chromosome_regions.append(chr_region)
-
             # Insert chromosome regions and get their IDs
             sleep_time = random.uniform(1, 10)
-            sleep(sleep_time)
+            # sleep(sleep_time)
             ChromosomeRegion.objects.bulk_create(chromosome_regions)
-
             # Create VaraDB_TFChipSeq objects with references to chromosome regions
             for chr_region, (_, row) in zip(chromosome_regions, chunked_df.iterrows()):
                 tf_chipseq = VaraDB_TFChipSeq(
@@ -43,22 +40,29 @@ def bulk_insert(chunk):
                     biosample_name=row["Biosample_name"],
                 )
                 tf_chipseq_objects.append(tf_chipseq)
-
             # Bulk create TF ChIP-seq objects
             sleep_time = random.uniform(1, 10)
-            sleep(sleep_time)
+            # sleep(sleep_time)
             VaraDB_TFChipSeq.objects.bulk_create(tf_chipseq_objects)
             return True
     except OperationalError as e:
+        print(e)
         sleep_time = random.uniform(20, 30)
-        sleep(sleep_time)
+        # sleep(sleep_time)
         return chunk
+    except Exception as e:
+        print(e)
 
 
 chunks = []
-chunk_size = 10000
+chunk_size = 5000
 for i in tqdm(range(0, len(df), chunk_size)):
     chunks.append([[i, min(i + chunk_size, len(df))]])
 
-with WorkerPool(n_jobs=50) as pool:
+for i in tqdm(range(0, len(df), chunk_size)):
+    bulk_insert([i, min(i + chunk_size, len(df))])
+
+with WorkerPool(n_jobs=70) as pool:
     results = pool.map(bulk_insert, chunks, progress_bar=True)
+
+repeat_again = [x for x in tqdm(results) if x is not True]
