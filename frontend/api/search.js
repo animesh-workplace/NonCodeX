5c78ae182250b65ea5182151f7f9277fe2e0aebe
Animesh Kumar Singh <animesh.workplace@gmail.com>
2025-01-29 16:59:18 +0530

    added file - frontend/api/search.js

---
+++ frontend/api/search.js
@@ -0,0 +1,30 @@
+export function useQuery() {
+	const config = useRuntimeConfig()
+	const BASEURL = `${config.public.API_BASE_URL}`
+
+	const uploadQuery = async (formData) => {
+		try {
+			// const csrfToken = useCookie('csrftoken')
+			console.log(BASEURL)
+
+			const { data, error } = await useFetch(`${BASEURL}/query/data/`, {
+				method: 'POST',
+				body: formData,
+				// headers: { 'X-CSRFToken': csrfToken.value },
+			})
+
+			if (error.value) {
+				throw new Error(error.value || 'An error occurred')
+			}
+
+			return data.value
+		} catch (err) {
+			console.error(err)
+			throw err
+		}
+	}
+
+	return {
+		uploadQuery,
+	}
+}
