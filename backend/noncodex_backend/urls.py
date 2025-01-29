3e5809d20f24bcc30d97c5d430fa57da9131b608
Animesh Kumar Singh <animesh.workplace@gmail.com>
2025-01-29 15:28:37 +0530

    updated file - backend/noncodex_backend/urls.py

--- backend/noncodex_backend/urls.py
+++ backend/noncodex_backend/urls.py
@@ -1,22 +1,30 @@
-"""
-URL configuration for noncodex_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+import os
+from dotenv import load_dotenv
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.urls import path, include
+
+load_dotenv(settings.BASE_DIR / ".env")
+
+admin.site.site_title = "NonCodeX"
+admin.site.index_title = "NonCodeX Admin Panel"
+admin.site.site_header = "[ NonCodeX Admin Panel ]"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(
+        f"{os.getenv('BASE_URL')}",
+        include(
+            [
+                path("admin/", admin.site.urls),
+                path(
+                    "api/",
+                    include(
+                        [
+                            path("query/", include("query_engine.urls")),
+                        ]
+                    ),
+                ),
+            ]
+        ),
+    )
 ]
