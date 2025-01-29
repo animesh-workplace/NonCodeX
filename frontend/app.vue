51a5b706729bef676d2783fc87e9ce17e9298010
Animesh Kumar Singh <animesh.workplace@gmail.com>
2025-01-29 16:59:12 +0530

    updated file - frontend/app.vue

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<div class="min-h-screen">
+		<Notivue v-slot="item">
+			<Notification :item="item" :theme="materialTheme" />
+		</Notivue>
+
+		<NuxtLayout>
+			<NuxtPage />
+		</NuxtLayout>
+	</div>
 </template>
+
+<script setup>
+import { materialTheme } from 'notivue'
+// import { useThemeStore } from '@/stores/theme'
+
+// const themeStore = useThemeStore()
+
+onMounted(() => {
+	// themeStore.loadTheme()
+})
+</script>
