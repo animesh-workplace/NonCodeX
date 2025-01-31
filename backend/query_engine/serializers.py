58687dab658e4ad0d02614fc7024b95a7e49977b
Animesh Kumar Singh <animesh.workplace@gmail.com>
2025-01-31 13:11:04 +0530

    updated file - backend/query_engine/serializers.py

--- backend/query_engine/serializers.py
+++ backend/query_engine/serializers.py
@@ -15,49 +15,49 @@ from .models import (
 class VaraDBTFChipSeqSerializer(serializers.ModelSerializer):
     class Meta:
         model = VaraDB_TFChipSeq
-        fields = "__all__"
+        exclude = ("id",)
 
 
 class VaraDBPromoterChromHMMSerializer(serializers.ModelSerializer):
     class Meta:
         model = VaraDB_Promoter_ChromHMM
-        fields = "__all__"
+        exclude = ("id",)
 
 
 class VaraDBPromoterTSSSerializer(serializers.ModelSerializer):
     class Meta:
         model = VaraDB_Promoter_TSS
-        fields = "__all__"
+        exclude = ("id",)
 
 
 class VaraDBSuperEnhancerSerializer(serializers.ModelSerializer):
     class Meta:
         model = VaraDB_SuperEnhancer
-        fields = "__all__"
+        exclude = ("id",)
 
 
 class VaraDBChromatinStateSerializer(serializers.ModelSerializer):
     class Meta:
         model = VaraDB_ChromatinState
-        fields = "__all__"
+        exclude = ("id",)
 
 
 class VaraDBEnhancerGROSeqSerializer(serializers.ModelSerializer):
     class Meta:
         model = VaraDB_Enhancer_GROSeq
-        fields = "__all__"
+        exclude = ("id",)
 
 
 class VaraDBEnhancerPROSeqSerializer(serializers.ModelSerializer):
     class Meta:
         model = VaraDB_Enhancer_PROSeq
-        fields = "__all__"
+        exclude = ("id",)
 
 
 class VaraDBEnhancerFANTOM5Serializer(serializers.ModelSerializer):
     class Meta:
         model = VaraDB_Enhancer_FANTOM5
-        fields = "__all__"
+        exclude = ("id",)
 
 
 class ChromosomeRegionSerializer(serializers.ModelSerializer):
@@ -74,4 +74,37 @@ class ChromosomeRegionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ChromosomeRegion
-        fields = "__all__"
+        exclude = ("id",)
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+
+        # Extract base fields
+        base_data = {
+            "chr": data.pop("chr", None),
+            "end": data.pop("end", None),
+            "type": data.pop("type", None),
+            "start": data.pop("start", None),
+        }
+
+        # Merge all related fields into a single list
+        merged_data = []
+        for key in [
+            "varadb_tf_chipseq",
+            "varadb_promoter_chrohmm",
+            "varadb_promoter_tss",
+            "varadb_superenhancer",
+            "varadb_chromatin_state",
+            "varadb_enhancer_groseq",
+            "varadb_enhancer_proseq",
+            "varadb_enhancer_fantom5",
+        ]:
+            if data[key]:  # Only include non-empty values
+                merged_data.extend(data[key])
+
+        # If only one related item exists, return a merged object
+        if len(merged_data) == 1:
+            return {**base_data, **merged_data[0]}
+
+        # If multiple related items exist, return an array
+        return [{**base_data, **item} for item in merged_data]
